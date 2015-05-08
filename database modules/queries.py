05ef936cce8ce425d39475bc62cad4a075400eac
wasifzaman <zelvenstormrage@gmail.com>
2015-05-08 18:22:25 -0400

    sync

--- database modules/queries.py
+++ database modules/queries.py
@@ -26,6 +26,35 @@ class Database:
 		self.cur = self.conn.cursor()
 		return {'state': 'success'}
 
+	def insert_(self, data, to_table):
+		sql_script = 'PRAGMA table_info(' + to_table + ')'
+		table_columns = [row[1] for row in self.cur.execute(sql_script)]
+		table_values = tuple([data[column_name] \
+			if column_name in data else '' for \
+			column_name in table_columns])
+		value_fill = str(tuple(['?' for i in table_columns])).replace("'", '')
+		return {'prefix': 'INSERT INTO ' + to_table + ' VALUES ' + value_fill, 'suffix': table_values}
+
+	def add_student(self, table_data):
+		sql_script = self.insert_(table_data, 'students')
+		self.cur.execute(sql_script['prefix'], sql_script['suffix'])
+		self.conn.commit()
+
+	def add_table(self, table_name, table_data):
+		for row in table_data:
+			sql_script = self.insert_(row, table_name)
+			self.cur.execute(sql_script['prefix'], sql_script['suffix'])
+			self.conn.commit()
+
+	def update_student(self, id, table_data):
+		return
+
+	def update_table(self, id, table_name, table_data):
+		return
+
 d = Database()
 #d.create_open_database(new_db=os.path.abspath(os.pardir) + '\database\\test.db', current=True)
 d.create_open_database()
+#d.add_student({'id': '0'})
+#d.add_table('payments', [{'id': '0', 'value': '500'}])
+#d.add_table('guardians', [{'id': '0', 'first': 'John'}])
